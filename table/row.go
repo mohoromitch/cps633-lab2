9ceb894e6d4064df77aa8e88cf8b533fc6e5e84b
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2016-10-29 11:40:23 -0400

    Added getters for row data

--- table/row.go
+++ table/row.go
@@ -8,3 +8,22 @@ type row struct {
 	secondDwellTime int
 }
 
+func (r *row) FirstKeyCode() int {
+	return r.firstKeyCode
+}
+
+func (r *row) SecondKeyCode() int {
+	return r.secondKeyCode
+}
+
+func (r *row) FlyTime() int {
+	return r.flyTime
+}
+
+func (r *row) FirstDwellTime() int {
+	return r.firstDwellTime
+}
+
+func (r *row) SecondDwellTime() int {
+	return r.secondDwellTime
+}
