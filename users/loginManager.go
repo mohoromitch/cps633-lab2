73636cb7449f8dcb4b8fcad3fa0b25377f3bfa72
Mitchell Mohorovich <mitchell.mohorovich@gmail.com>
2016-10-31 22:13:45 -0400

    Added base login input functionality

--- users/loginManager.go
+++ users/loginManager.go
@@ -17,8 +17,28 @@ func NewLoginManager(filename string) *loginManager {
 }
 
 func (lm *loginManager) Login() int {
-	var username string
-	fmt.Print("Please enter in your username")
-	fmt.Scanf("%s\n", &username)
+	var username, file, permission string
+	username = lm.readUsername()
+	file = lm.readFile()
+	permission = lm.readPermission()
+	fmt.Println("Requesting file " + file + " with permission " + permission + " for user " + username)
 	return 0
+}
+
+func (lm *loginManager) readUsername() (username string) {
+	fmt.Println("Please enter in your username")
+	fmt.Scanf("%s\n", &username)
+	return username
+}
+
+func (lm *loginManager) readFile() (file string) {
+	fmt.Println("Please enter in the file you want to access")
+	fmt.Scanf("%s\n", &file)
+	return file
+}
+
+func (lm *loginManager) readPermission() (permission string) {
+	fmt.Println("Please enter in the permission you would like to request")
+	fmt.Scanf("%s\n", &permission)
+	return permission
 }
